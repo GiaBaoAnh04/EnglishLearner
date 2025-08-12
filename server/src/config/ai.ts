6e5f9685bab232c5e4d133bc6966f78a12b8bb70
GiaBaoAnh04 <giabaoanh3003@gmail.com>
2025-08-12 21:51:23 +0700

    fix config ai

--- server/src/config/ai.ts
+++ server/src/config/ai.ts
@@ -1,6 +1,4 @@
 // src/config.ts
 export const config = {
-  openAIKey:
-    process.env.OPENAI_API_KEY ||
-    "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",
+  openAIKey: process.env.OPENAI_API_KEY || "",
 };
